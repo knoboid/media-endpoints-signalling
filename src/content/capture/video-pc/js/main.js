9f2450a4f043814d9f167d3abca8b4d45c42fc89
MJ Bingley <kn_b_y@yahoo.co.uk>
2024-04-22 13:14:56 +0100

    Editor formatting changes only

--- src/content/capture/video-pc/js/main.js
+++ src/content/capture/video-pc/js/main.js
@@ -1,15 +1,15 @@
 /*
-*  Copyright (c) 2016 The WebRTC project authors. All Rights Reserved.
-*
-*  Use of this source code is governed by a BSD-style license
-*  that can be found in the LICENSE file in the root of the source
-*  tree.
-*/
+ *  Copyright (c) 2016 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
 
-'use strict';
+"use strict";
 
-const leftVideo = document.getElementById('leftVideo');
-const rightVideo = document.getElementById('rightVideo');
+const leftVideo = document.getElementById("leftVideo");
+const rightVideo = document.getElementById("rightVideo");
 
 let stream;
 
@@ -17,7 +17,7 @@ let pc1;
 let pc2;
 const offerOptions = {
   offerToReceiveAudio: 1,
-  offerToReceiveVideo: 1
+  offerToReceiveVideo: 1,
 };
 
 let startTime;
@@ -28,22 +28,24 @@ function maybeCreateStream() {
   }
   if (leftVideo.captureStream) {
     stream = leftVideo.captureStream();
-    console.log('Captured stream from leftVideo with captureStream',
-        stream);
+    console.log("Captured stream from leftVideo with captureStream", stream);
     call();
   } else if (leftVideo.mozCaptureStream) {
     stream = leftVideo.mozCaptureStream();
-    console.log('Captured stream from leftVideo with mozCaptureStream()',
-        stream);
+    console.log(
+      "Captured stream from leftVideo with mozCaptureStream()",
+      stream
+    );
     call();
   } else {
-    console.log('captureStream() not supported');
+    console.log("captureStream() not supported");
   }
 }
 
 // Video tag capture must be set up after video tracks are enumerated.
 leftVideo.oncanplay = maybeCreateStream;
-if (leftVideo.readyState >= 3) { // HAVE_FUTURE_DATA
+if (leftVideo.readyState >= 3) {
+  // HAVE_FUTURE_DATA
   // Video is already ready to play, call maybeCreateStream in case oncanplay
   // fired before we registered the event handler.
   maybeCreateStream();
@@ -52,22 +54,26 @@ if (leftVideo.readyState >= 3) { // HAVE_FUTURE_DATA
 leftVideo.play();
 
 rightVideo.onloadedmetadata = () => {
-  console.log(`Remote video videoWidth: ${rightVideo.videoWidth}px,  videoHeight: ${rightVideo.videoHeight}px`);
+  console.log(
+    `Remote video videoWidth: ${rightVideo.videoWidth}px,  videoHeight: ${rightVideo.videoHeight}px`
+  );
 };
 
 rightVideo.onresize = () => {
-  console.log(`Remote video size changed to ${rightVideo.videoWidth}x${rightVideo.videoHeight}`);
+  console.log(
+    `Remote video size changed to ${rightVideo.videoWidth}x${rightVideo.videoHeight}`
+  );
   // We'll use the first onresize callback as an indication that
   // video has started playing out.
   if (startTime) {
     const elapsedTime = window.performance.now() - startTime;
-    console.log('Setup time: ' + elapsedTime.toFixed(3) + 'ms');
+    console.log("Setup time: " + elapsedTime.toFixed(3) + "ms");
     startTime = null;
   }
 };
 
 function call() {
-  console.log('Starting call');
+  console.log("Starting call");
   startTime = window.performance.now();
   const videoTracks = stream.getVideoTracks();
   const audioTracks = stream.getAudioTracks();
@@ -79,20 +85,24 @@ function call() {
   }
   const servers = null;
   pc1 = new RTCPeerConnection(servers);
-  console.log('Created local peer connection object pc1');
-  pc1.onicecandidate = e => onIceCandidate(pc1, e);
+  console.log("Created local peer connection object pc1");
+  pc1.onicecandidate = (e) => onIceCandidate(pc1, e);
   pc2 = new RTCPeerConnection(servers);
-  console.log('Created remote peer connection object pc2');
-  pc2.onicecandidate = e => onIceCandidate(pc2, e);
-  pc1.oniceconnectionstatechange = e => onIceStateChange(pc1, e);
-  pc2.oniceconnectionstatechange = e => onIceStateChange(pc2, e);
+  console.log("Created remote peer connection object pc2");
+  pc2.onicecandidate = (e) => onIceCandidate(pc2, e);
+  pc1.oniceconnectionstatechange = (e) => onIceStateChange(pc1, e);
+  pc2.oniceconnectionstatechange = (e) => onIceStateChange(pc2, e);
   pc2.ontrack = gotRemoteStream;
 
-  stream.getTracks().forEach(track => pc1.addTrack(track, stream));
-  console.log('Added local stream to pc1');
+  stream.getTracks().forEach((track) => pc1.addTrack(track, stream));
+  console.log("Added local stream to pc1");
 
-  console.log('pc1 createOffer start');
-  pc1.createOffer(onCreateOfferSuccess, onCreateSessionDescriptionError, offerOptions);
+  console.log("pc1 createOffer start");
+  pc1.createOffer(
+    onCreateOfferSuccess,
+    onCreateSessionDescriptionError,
+    offerOptions
+  );
 }
 
 function onCreateSessionDescriptionError(error) {
@@ -102,11 +112,19 @@ function onCreateSessionDescriptionError(error) {
 function onCreateOfferSuccess(desc) {
   console.log(`Offer from pc1
 ${desc.sdp}`);
-  console.log('pc1 setLocalDescription start');
-  pc1.setLocalDescription(desc, () => onSetLocalSuccess(pc1), onSetSessionDescriptionError);
-  console.log('pc2 setRemoteDescription start');
-  pc2.setRemoteDescription(desc, () => onSetRemoteSuccess(pc2), onSetSessionDescriptionError);
-  console.log('pc2 createAnswer start');
+  console.log("pc1 setLocalDescription start");
+  pc1.setLocalDescription(
+    desc,
+    () => onSetLocalSuccess(pc1),
+    onSetSessionDescriptionError
+  );
+  console.log("pc2 setRemoteDescription start");
+  pc2.setRemoteDescription(
+    desc,
+    () => onSetRemoteSuccess(pc2),
+    onSetSessionDescriptionError
+  );
+  console.log("pc2 createAnswer start");
   // Since the 'remote' side has no media stream we need
   // to pass in the right constraints in order for it to
   // accept the incoming offer of audio and video.
@@ -128,28 +146,36 @@ function onSetSessionDescriptionError(error) {
 function gotRemoteStream(event) {
   if (rightVideo.srcObject !== event.streams[0]) {
     rightVideo.srcObject = event.streams[0];
-    console.log('pc2 received remote stream', event);
+    console.log("pc2 received remote stream", event);
   }
 }
 
 function onCreateAnswerSuccess(desc) {
   console.log(`Answer from pc2:
 ${desc.sdp}`);
-  console.log('pc2 setLocalDescription start');
-  pc2.setLocalDescription(desc, () => onSetLocalSuccess(pc2), onSetSessionDescriptionError);
-  console.log('pc1 setRemoteDescription start');
-  pc1.setRemoteDescription(desc, () => onSetRemoteSuccess(pc1), onSetSessionDescriptionError);
+  console.log("pc2 setLocalDescription start");
+  pc2.setLocalDescription(
+    desc,
+    () => onSetLocalSuccess(pc2),
+    onSetSessionDescriptionError
+  );
+  console.log("pc1 setRemoteDescription start");
+  pc1.setRemoteDescription(
+    desc,
+    () => onSetRemoteSuccess(pc1),
+    onSetSessionDescriptionError
+  );
 }
 
 function onIceCandidate(pc, event) {
-  getOtherPc(pc).addIceCandidate(event.candidate)
-      .then(
-          () => onAddIceCandidateSuccess(pc),
-          err => onAddIceCandidateError(pc, err)
-      );
+  getOtherPc(pc)
+    .addIceCandidate(event.candidate)
+    .then(
+      () => onAddIceCandidateSuccess(pc),
+      (err) => onAddIceCandidateError(pc, err)
+    );
   console.log(`${getName(pc)} ICE candidate: 
-${event.candidate ?
-    event.candidate.candidate : '(null)'}`);
+${event.candidate ? event.candidate.candidate : "(null)"}`);
 }
 
 function onAddIceCandidateSuccess(pc) {
@@ -157,20 +183,22 @@ function onAddIceCandidateSuccess(pc) {
 }
 
 function onAddIceCandidateError(pc, error) {
-  console.log(`${getName(pc)} failed to add ICE Candidate: ${error.toString()}`);
+  console.log(
+    `${getName(pc)} failed to add ICE Candidate: ${error.toString()}`
+  );
 }
 
 function onIceStateChange(pc, event) {
   if (pc) {
     console.log(`${getName(pc)} ICE state: ${pc.iceConnectionState}`);
-    console.log('ICE state change event: ', event);
+    console.log("ICE state change event: ", event);
   }
 }
 
 function getName(pc) {
-  return (pc === pc1) ? 'pc1' : 'pc2';
+  return pc === pc1 ? "pc1" : "pc2";
 }
 
 function getOtherPc(pc) {
-  return (pc === pc1) ? pc2 : pc1;
+  return pc === pc1 ? pc2 : pc1;
 }
