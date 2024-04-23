0932df92623f305910322fd9ed16b7f676779e31
MJ Bingley <kn_b_y@yahoo.co.uk>
2024-04-23 13:11:53 +0100

    Removed logging

--- src/content/capture/video-pc/js/connection-registry.js
+++ src/content/capture/video-pc/js/connection-registry.js
@@ -13,19 +13,11 @@ class ConnectionRegistry {
   }
 
   getOtherPeer(name) {
-    console.log(name);
-    console.log(connections[0]);
     for (const connection of connections) {
-      // console.log(connection);
       if (name === connection.from) {
-        console.log("fromname match");
-        console.log(`other peer name: ${connection.to}`);
         console.log(this.peerRegistry.getPeer(connection.to));
         return this.peerRegistry.getPeer(connection.to);
       } else if (name === connection.to) {
-        console.log("toname match");
-        console.log(`other peer name: ${connection.from}`);
-        console.log(this.peerRegistry.getPeer(connection.from));
         return this.peerRegistry.getPeer(connection.from);
       }
     }
