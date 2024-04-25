d8a0a6c948489169f5c767a479e8ef82d5d03bf6
MJ Bingley <kn_b_y@yahoo.co.uk>
2024-04-25 10:39:03 +0100

    Tidy

--- src/content/capture/video-pc/js/peer-connect.js
+++ src/content/capture/video-pc/js/peer-connect.js
@@ -3,17 +3,13 @@ import PeerRegistry from "./peer-registrty.js";
 import ConnectionRegistry from "./connection-registry.js";
 import { createCallerConnection } from "./connections/caller-connection.js";
 import { createResponderConnection } from "./connections/responder-connection.js";
-import SignallingChannel from "./signalling/same-page/signalling-channel.js";
-import CallerSignaller from "./signalling/same-page/caller-signaller.js";
-import ResponderSignaller from "./signalling/same-page/responder-signaller.js";
 import NWCallerSignaller from "./signalling/network/caller-signaller.js";
 import NWResponderSignaller from "./signalling/network/responder-signaller.js";
-
 import RespondersList from "./ui/responders-list.js";
 
 // const wss = "wss://localhost:5501";
-// const wss = "wss://192.168.43.35:5501";
-const wss = "wss://192.168.0.72:5501";
+const wss = "wss://192.168.43.35:5501";
+// const wss = "wss://192.168.0.72:5501";
 
 let callerID, responderID;
 
@@ -23,21 +19,15 @@ const rightVideo = document.getElementById("rightVideo");
 const callersList = document.querySelector("#callers");
 const respondersList = document.querySelector("#responders");
 
-// const callerIDElement = document.querySelector("#caller-id");
 const responderIDElement = document.querySelector("#responder-id");
 
 const respondersComponent = new RespondersList(respondersList);
-
-const signallingChannel = new SignallingChannel();
-const callerSignaller = new CallerSignaller(signallingChannel);
-const responderSignaller = new ResponderSignaller(signallingChannel);
 
 const nWCallerSignaller = new NWCallerSignaller(wss);
 const nWResponderSignaller = new NWResponderSignaller(wss);
 
 nWCallerSignaller.addEventListener("onGotCallerID", (e) => {
   callerID = e.data;
-  // callerIDElement.innerHTML = callerID;
 });
 
 nWResponderSignaller.addEventListener("onGotResponderID", (e) => {
@@ -78,26 +68,16 @@ export function peerConnect(stream) {
 
   respondersComponent.addEventListener("hangup", (e) => {
     console.log("hangup pressed");
-    // console.log(e.data);
-    // nWCallerSignaller.send({
-    //   type: "initiateCall",
-    //   payload: { responderID: e.data },
-    // });
   });
 
   nWResponderSignaller.addEventListener("initiateResponse", () => {
-    console.log("wrwre");
     recieve();
   });
 
   nWCallerSignaller.addEventListener("initiateCallSuccess", (event) => {
-    console.log("event.data");
+    console.log("get initiateCallSuccess");
     console.log(event.data);
     call();
-    // const otherResponders = event.data.filter(
-    //   (responder) => Number(responder.id) !== responderID
-    // );
-    // respondersComponent.render(otherResponders);
   });
 
   let pc1, caller;
@@ -109,12 +89,12 @@ export function peerConnect(stream) {
 
   let startTime;
 
-  // Video tag capture must be set up after video tracks are enumerated.
-  // leftVideo.oncanplay = call;
+  leftVideo.oncanplay = () => {
+    console.log("left vid - on can play triggered");
+    //call()
+  };
   if (leftVideo.readyState >= 3) {
-    // HAVE_FUTURE_DATA
-    // Video is already ready to play, call maybeCreateStream in case oncanplay
-    // fired before we registered the event handler.
+    console.log("left vid - readyState >= 3");
     // call();
   }
 
@@ -124,19 +104,6 @@ export function peerConnect(stream) {
     console.log(
       `Remote video videoWidth: ${rightVideo.videoWidth}px,  videoHeight: ${rightVideo.videoHeight}px`
     );
-  };
-
-  rightVideo.onresize = () => {
-    console.log(
-      `Remote video size changed to ${rightVideo.videoWidth}x${rightVideo.videoHeight}`
-    );
-    // We'll use the first onresize callback as an indication that
-    // video has started playing out.
-    if (startTime) {
-      const elapsedTime = window.performance.now() - startTime;
-      console.log("Setup time: " + elapsedTime.toFixed(3) + "ms");
-      startTime = null;
-    }
   };
 
   function call() {
@@ -152,22 +119,14 @@ export function peerConnect(stream) {
     }
     const servers = null;
 
-    // caller = createCallerConnection(servers, p1, stream, callerSignaller);
     caller = createCallerConnection(servers, p1, stream, nWCallerSignaller);
     pc1 = caller;
     peers.setConnection(p1, pc1);
     console.log("Created local peer connection object pc1");
-
-    // responder = createResponderConnection(servers, p2, responderSignaller);
-    // responder = createResponderConnection(servers, p2, nWResponderSignaller);
-    // pc2 = responder;
-    // peers.setConnection(p2, pc2);
-    // console.log("Created remote peer connection object pc2");
   }
 
   function recieve() {
     const servers = null;
-
     responder = createResponderConnection(servers, p2, nWResponderSignaller);
     pc2 = responder;
     peers.setConnection(p2, pc2);
