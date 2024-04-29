60b529b15867e912700b2dc6a0b60dbb8ab5518f
MJ Bingley <kn_b_y@yahoo.co.uk>
2024-04-29 12:50:19 +0100

    Set ws ip address

--- src/content/capture/video-pc/js/main.js
+++ src/content/capture/video-pc/js/main.js
@@ -14,8 +14,8 @@ const constraints = {
 };
 
 // const wss = "wss://localhost:5501";
-// const wss = "wss://192.168.43.35:5501";
-const wss = "wss://192.168.0.72:5502";
+const wss = "wss://192.168.43.35:5502";
+// const wss = "wss://192.168.0.72:5502";
 // const wss = "wss://192.168.0.72:5501";
 const servers = null;
 
--- src/content/capture/video-pc/js/switchboard.js
+++ src/content/capture/video-pc/js/switchboard.js
@@ -1,8 +1,8 @@
 import { adminSetup } from "./admin.js";
 
 // const wss = "wss://localhost:5501";
-// const wss = "wss://192.168.43.35:5501";
-const wss = "wss://192.168.0.72:5502";
+const wss = "wss://192.168.43.35:5502";
+// const wss = "wss://192.168.0.72:5502";
 // const wss = "wss://192.168.0.72:5501";
 
 adminSetup(wss);
