4495b4d0706a480950ba10f17af7330f2b697c5c
MJ Bingley <kn_b_y@yahoo.co.uk>
2024-05-07 13:25:14 +0100

    Fix error.

--- src/content/capture/video-pc/js/create-transmitter.js
+++ src/content/capture/video-pc/js/create-transmitter.js
@@ -2,9 +2,9 @@ import Transmitter from "./media-transmitter.js";
 
 // Shoiuld it be createUserMediaTransmitter
 
-export function createTransmitter(servers, constraints, ui, code) {
+export function createTransmitter(servers, constraints, code) {
   function setup(stream) {
-    const transmitter = new Transmitter(servers, stream, ui, code);
+    const transmitter = new Transmitter(servers, stream, code);
     return new Promise((resolve, reject) => resolve(transmitter));
   }
   return navigator.mediaDevices.getUserMedia(constraints).then(setup); //.catch(handleError);
