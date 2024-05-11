f14cced8c4a5bcd8314c2d6b28552a4d862c8a07
MJ Bingley <kn_b_y@yahoo.co.uk>
2024-05-11 15:39:39 +0100

    Change IP address.

--- src/client/js/signalling/network/create-ws-signallers.js
+++ src/client/js/signalling/network/create-ws-signallers.js
@@ -6,9 +6,9 @@ import RecieverSignaller from "./reciever-signaller.js";
 
 // const wss = "wss://localhost:5501";
 // const wss = "wss://127.0.0.1:5502";
-const wss = "wss://192.168.43.35:5502";
+// const wss = "wss://192.168.43.35:5502";
 // const wss = "wss://192.168.43.17:5502";
-// const wss = "wss://192.168.0.72:5502";
+const wss = "wss://192.168.0.72:5502";
 // const wss = "wss://192.168.0.72:5501";
 
 export function createUserWSSignaller() {
