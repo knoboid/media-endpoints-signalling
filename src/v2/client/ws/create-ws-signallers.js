0467d243ec7eb457c9d4d84694d1afc1be943fc1
MJ Bingley <kn_b_y@yahoo.co.uk>
2024-05-20 10:54:18 +0100

    Changed IP address.

--- src/v2/client/ws/create-ws-signallers.js
+++ src/v2/client/ws/create-ws-signallers.js
@@ -1,7 +1,7 @@
 import TransmitterSignaller from "./transmitter-signaller.js";
 import ReceiverSignaller from "./receiver-signaller.js";
 
-const wss = "wss://192.168.43.35:5502";
+const wss = "wss://192.168.0.72:5502";
 
 export function createTransmitterWSSignaller(code) {
   return new TransmitterSignaller(wss);
