2af981f5d0e81d9a943501d2424dbd6cb477d781
MJ Bingley <kn_b_y@yahoo.co.uk>
2024-05-23 10:53:43 +0100

    Added functions to client-model/create-client.js

--- src/v2/server/clients-model/create-client.js
+++ src/v2/server/clients-model/create-client.js
@@ -1,11 +1,33 @@
+import ClientGroups from "./groups/client-groups.js";
+import Connections from "./groups/connections.js";
+import WebSocketClient from "./client/WebSocketClient.js";
 import Endpoint from "./client/Endpoint.js";
 
 let id = 1;
 
-export function createReceiver(webSocket) {
-  return new Endpoint(id++, "receiver", webSocket);
-}
+export const clientGroups = new ClientGroups();
+export const connections = new Connections(clientGroups);
 
 export function createTransmitter(webSocket) {
-  return new Endpoint(id++, "transmitter", webSocket);
+  return clientGroups.addClient(new Endpoint(id++, "transmitter", webSocket));
+}
+
+export function createReceiver(webSocket) {
+  return clientGroups.addClient(new Endpoint(id++, "receiver", webSocket));
+}
+
+export function createDataViewer(webSocket) {
+  return clientGroups.addClient(
+    new WebSocketClient(id++, "dataViewer", webSocket)
+  );
+}
+
+export function getClient(clientId) {
+  const clientGroup = clientGroups.clientIdMap[clientId];
+  return clientGroup.getClient(clientId);
+}
+
+export function deleteClient(clientId) {
+  const clientGroup = clientGroups.clientIdMap[clientId];
+  return clientGroup.removeClient(clientId);
 }
